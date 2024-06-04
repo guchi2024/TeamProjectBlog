484a650accc27c8099144d3b0c0cae934228c2d1
Robbie <chihyeongu85@gmail.com>
2024-06-04 11:39:08 +0900

    init commit

---
+++ src/main/java/com/sparta/projectblog/ProjectBlogApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.projectblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/projectblog/ProjectBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.projectblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
