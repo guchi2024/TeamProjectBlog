1afae2619b83d9b1b5b774075491284b699e0374
oyshrimp <oyshrimp@naver.com>
2024-06-10 18:00:18 +0900

    Update FeedUpdateRequestDto.java

--- src/main/java/com/sparta/projectblog/dto/FeedUpdateRequestDto.java
+++ src/main/java/com/sparta/projectblog/dto/FeedUpdateRequestDto.java
@@ -4,11 +4,7 @@ import java.time.LocalDateTime;
 
 public class FeedUpdateRequestDto {
     private String content;
-    private LocalDateTime updatedAt;
 
-    public FeedUpdateRequestDto(String content) {
-        this.content = content;
-    }
     public String getContent() {
         return content;
     }
